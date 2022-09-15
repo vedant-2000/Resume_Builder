dac086eef409a4dcd3ab19718cae8e20d1bfd238
Vedant Joshi <vedant2000joshi@gmail.com>
2022-09-15 02:39:20 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-
-import Comp1 from './Comp1';
-import {BrowserRouter as Router,Routes, Route, Link} from 'react-router-dom';
-import Home from './Home';
-import Comp2 from './Comp2';
+import Comp1 from "./Comp1";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Comp2 from "./Comp2";
 function App() {
-  alert("Please Full Resume Details Then Click Submit Otherwise All Data Get Erased")
   return (
-  
-   <>
-    <Routes>
-    <Route exact path="/" element = {<Home/>}></Route>
-    <Route exact path="/resume" element = {<Comp1/>}></Route>
-    <Route exact path="/tobeprinted" element = {<Comp2/>}></Route>
-  </Routes>
-      
-  </>
-      
-
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/resume" element={<Comp1 />}></Route>
+        <Route exact path="/tobeprinted" element={<Comp2 />}></Route>
+      </Routes>
+    </>
   );
 }
 
